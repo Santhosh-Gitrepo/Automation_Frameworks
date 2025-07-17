b2eb02f9b8b677039b6b5bdd5bb705b5f3c1e0d1
Santhosh R <san.sathish.com@gmail.com>
2025-07-18 01:31:18 +0530

    Updated the script

--- src/test/java/TestScripts/TestScript1.java
+++ src/test/java/TestScripts/TestScript1.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.Test;
 
 import Utils.TestLogger;
@@ -16,6 +17,12 @@ public void test() throws IOException, InterruptedException
 {
 	TestLogger logger = new TestLogger();
 	WebDriverManager.chromedriver().setup();;
+	// Configure ChromeOptions
+    ChromeOptions options = new ChromeOptions();
+    options.setBinary("C:\\Users\\ASUS\\AppData\\Local\\Google\\Chrome\\Application\\chrome.exe"); // <-- your path
+    options.addArguments("--headless");       // Headless mode for Jenkins
+    options.addArguments("--disable-gpu");
+    options.addArguments("--window-size=1920,1080");
 	WebDriver driver = new ChromeDriver();
 	driver.get("https://www.facebook.com");
 	driver.close();
