9129650fb787c1cfe54a0056f519e40ed3835e9a
Santhosh R <san.sathish.com@gmail.com>
2025-07-18 00:45:37 +0530

    Initial Commit - New Project

---
+++ src/test/java/TestScripts/TestScript1.java
@@ -0,0 +1,24 @@
+package TestScripts;
+
+import java.io.IOException;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import Utils.TestLogger;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class TestScript1 
+{
+@Test
+public void test() throws IOException, InterruptedException
+{
+	TestLogger logger = new TestLogger();
+	WebDriverManager.chromedriver();
+	WebDriver driver = new ChromeDriver();
+	driver.get("https://www.facebook.com");
+	driver.close();
+	logger.log("WebPage is Launched and Closed");
+}
+}
